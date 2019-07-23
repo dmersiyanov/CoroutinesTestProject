92dddd720d744430b6065ea655be443f4d8e1656
Dmitry Mersiyanov <dmersiyanov@yandex.ru>
2019-07-23 22:19:33 +0300

    added working network calls

--- app/src/main/java/com/dmity/courutinesotus/MainActivity.kt
+++ app/src/main/java/com/dmity/courutinesotus/MainActivity.kt
@@ -2,6 +2,13 @@ package com.dmity.courutinesotus
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.dmity.courutinesotus.models.FilmsResponse
+import com.dmity.courutinesotus.network.Api
+import com.dmity.courutinesotus.network.CountriesService
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,11 +16,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        unirest.get("https://restcountries-v1.p.rapidapi.com/all")
-//            .header("X-RapidAPI-Host", "restcountries-v1.p.rapidapi.com")
-//            .header("X-RapidAPI-Key", "3d24c47c88msh44b5be0ebd52ffbp194530jsn65a37bef31e4")
-//            .end(function (result) {
-//                console.log(result.status, result.headers, result.body);
-//            });
+        val service = Api().getInstance()?.create(CountriesService::class.java)
+
+
+        rvFilmsList.setOnClickListener {
+            service?.getCountries(CountriesService.API_KEY)
+                ?.enqueue(object : Callback<FilmsResponse> {
+                    override fun onFailure(call: Call<FilmsResponse>, t: Throwable) {
+                        println(t.message)
+                    }
+
+                    override fun onResponse(call: Call<FilmsResponse>, response: Response<FilmsResponse>) {
+                        println(response)
+                    }
+                })
+        }
+
+
+
     }
 }
